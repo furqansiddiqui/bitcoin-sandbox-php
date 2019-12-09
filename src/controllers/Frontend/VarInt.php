1f7a2d81fe5c10010d0f4b272aad2239e3f02eb7
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-12-09 20:59:54 +0500

    template updates

---
+++ src/controllers/Frontend/VarInt.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controllers\Frontend;
+
+use App\Kernel\FrontendController;
+
+/**
+ * Class VarInt
+ * @package App\Controllers\Frontend
+ */
+class VarInt extends FrontendController
+{
+
+}
